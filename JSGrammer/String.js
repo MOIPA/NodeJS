a53432f678ec3f3421a3df1f580396f48a664dc9
MOIPA <tassassin@sina.com>
2019-01-17 15:36:34 +0800

    basic js

---
+++ JSGrammer/String.js
@@ -0,0 +1,16 @@
+let string1 = 'hello';
+let string2 = "world";
+process.stdout.write(string1+string2+'\n');
+
+let string3 = "I\'m ok";
+console.log(string3);
+
+let string4 = `this is
+my home`;
+console.log(string4);
+
+console.log(`${string1} ${string2}`);
+
+console.log(string1.toUpperCase());
+console.log(string1.indexOf('llo'));
+console.log(string1.substring(1,3));
